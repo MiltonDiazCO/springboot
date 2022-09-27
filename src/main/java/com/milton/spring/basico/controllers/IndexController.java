c384094630c96d8e30a5f25af56d2bbc6035ce69
MiltonDiazCO <diazmartinezmilton@gmail.com>
2022-09-27 16:51:03 -0500

    Anotacion @RequestMapping sobre el controlador

--- src/main/java/com/milton/spring/basico/controllers/IndexController.java
+++ src/main/java/com/milton/spring/basico/controllers/IndexController.java
@@ -6,12 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
-	@GetMapping({ "/", "/index" })
+	@GetMapping({ "/", "", "/index" })
 	public String index(Model model) {
 		model.addAttribute("parrafo1", "Hola, soy el parrafo No. 1");
 		return "index";
