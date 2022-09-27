fb515cf78d31a8c46086c45b52a4015bc5bce939
MiltonDiazCO <diazmartinezmilton@gmail.com>
2022-09-27 16:45:57 -0500

    Pasando datos a la vista

--- src/main/java/com/milton/spring/basico/controllers/IndexController.java
+++ src/main/java/com/milton/spring/basico/controllers/IndexController.java
@@ -1,21 +1,39 @@
 package com.milton.spring.basico.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
 	@GetMapping({ "/", "/index" })
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("parrafo1", "Hola, soy el parrafo No. 1");
 		return "index";
 	}
 
-	@RequestMapping(value = "/index2", method = RequestMethod.GET)
-	public String indexTwo() {
+	@GetMapping({ "/index2" })
+	public String indexTwo(ModelMap model) {
+		model.addAttribute("parrafo2", "Hola, soy el parrafo No. 2");
 		return "index";
+	}
+
+	@GetMapping({ "/index3" })
+	public String indexThree(Map<String, Object> map) {
+		map.put("parrafo3", "Hola, soy el parrafo No. 3");
+		return "index";
+	}
+
+	@GetMapping({ "/index4" })
+	public ModelAndView indexFour(ModelAndView mv) {
+		mv.addObject("parrafo3", "Hola, soy el parrafo No. 4");
+		mv.setViewName("index");
+		return mv;
 	}
 
 }
