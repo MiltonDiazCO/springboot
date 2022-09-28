d92a246a17c694d133cf6226df202c60ad6e1c2b
MiltonDiazCO <diazmartinezmilton@gmail.com>
2022-09-28 18:36:40 -0500

    Anotacion @PathVariable, con multiples parametros en la URL

--- src/main/java/com/milton/spring/basico/controllers/ParamsController.java
+++ src/main/java/com/milton/spring/basico/controllers/ParamsController.java
@@ -3,6 +3,7 @@ package com.milton.spring.basico.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,6 +20,18 @@ public class ParamsController {
 	@GetMapping({ "/mix-params" })
 	public String datosPersona(@RequestParam String nombre, @RequestParam int edad, Model model) {
 		model.addAttribute("datos2", ("Nombre: " + nombre + ", Edad: " + edad));
+		return "params";
+	}
+
+	@GetMapping({ "/variables/{nombre}" })
+	public String datosPersonaVariable(@PathVariable String nombre, Model model) {
+		model.addAttribute("datos3", ("Hola " + nombre));
+		return "params";
+	}
+
+	@GetMapping({ "/mix-variables/{nombre}/{edad}" })
+	public String datosPersonaVariable(@PathVariable String nombre, @PathVariable int edad, Model model) {
+		model.addAttribute("datos4", ("Nombre: " + nombre + ", Edad: " + edad));
 		return "params";
 	}
 
